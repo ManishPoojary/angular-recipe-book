faf8fb6025e4bed4c1957336063e02480b1a2410
ManishPoojary <manissh2327@gmail.com>
2019-04-09 15:42:28 +0530

    Adding a Shopping List Edit Section

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -7,7 +7,9 @@ import { Recipe } from '../recipe.model';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
-  recipes: Recipe[] = [];
+  recipes: Recipe[] = [
+    new Recipe('Bread ','Test','https://www.kroger.com/asset/5bfbfe3b9da07c0aa601e6bb?data=1')
+  ];
   constructor() { }
 
   ngOnInit() {
